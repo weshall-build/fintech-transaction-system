fe906f1e1e5b7accbf9a22e44289a5d40f01b0b1
vishal <vishalhatti2002@gmail.com>
2026-05-04 17:41:34 +0530

    initial transaction project setup

---
+++ src/main/java/com/payment/transactionTracker/TransactionTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.payment.transactionTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/payment/transactionTracker/TransactionTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payment.transactionTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
